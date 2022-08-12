e62b295bc05112352bfd4cca6d716d85a4312541
ibrahimcakmakk <ibrahim.cakmak@stu.fsm.edu.tr>
2022-08-12 17:15:39 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("bir sayı giriniz");
+            int sayi = Convert.ToInt32(Console.ReadLine);
+            Console.WriteLine("girmiş olduğunuz sayı : " + sayi);
+        }
+        catch (Exception ex)
+        {
+
+            Console.WriteLine("hata: " + ex.Message.ToString());
+        }
+        finally
+        {
+            Console.Write("işlem tamamlandı");
+
+        }
+
+        try
+        {
+            int a = int.Parse("null");
+        }
+        catch (ArgumentNullException ex)
+        {
+            Console.WriteLine("boş değer girdiniz");
+
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("veri tipi uygun değil");
+        }
+        catch (OverflowException ex)
+        {
+            Console.WriteLine("çok büyük veya çok küçük sayı girdiniz");
+        }
+        finally
+        {
+            Console.WriteLine("işlem başarılı");
+        }
+    }
+}
